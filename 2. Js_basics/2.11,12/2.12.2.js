38b31f38cb495c6cad26efb929e3ae9a7342678e
enya <essidanaya@gmail.com>
2019-04-08 23:53:58 +0300

    2.11, 2.12.1-2.12.4 was completed 2.12.4 ??

---
+++ 2. Js_basics/2.11,12/2.12.2.js
@@ -0,0 +1,11 @@
+var number = prompt ('Enter your number', '')
+
+if (number > 0) {
+    alert(1)
+}
+    else if (number < 0) {
+        alert(-1)
+}
+    else {
+        alert(0)
+}
---
+++ 2. Js_basics/2.11,12/2.12.4.js
@@ -0,0 +1,4 @@
+var a = prompt('enter the value of a', 0)
+var b = prompt('enter the value of b', 0)
+var result = a + b
+result = (a + b < 4) ? alert('a few') : alert('a lot')
