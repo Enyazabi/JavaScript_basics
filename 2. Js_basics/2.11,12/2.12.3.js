52892bd0627f2189ee3de2e0a7ee98f15a784180
Enyazabi <essidanaya@gmail.com>
2019-04-16 23:09:30 +0300

    2.12.3 complete

--- 2. Js_basics/2.11,12/2.12.3.js
+++ 2. Js_basics/2.11,12/2.12.3.js
@@ -3,6 +3,24 @@ var username = prompt('Кто?', '')
 
 
 if (username== 'Конь в пальто') {
-    var password = prompt('введите пароль','')
-}
 
+    var password = prompt('введите пароль','')
+
+    if (password = 'hui') {
+
+        alert ('Дратути')
+
+    } else {
+
+        alert ('Дотвидания')
+    }
+
+} else if (username == null) {
+
+    alert('введите погоняло')
+
+} else {
+
+    alert('ты не пройдёшь!')
+
+}
