88a54defdb63d7098665d15dcd480b50ab8f794a
Enyazabi <essidanaya@gmail.com>
2019-04-17 22:23:24 +0300

    a few a lot

--- 2. Js_basics/2.11,12/2.12.4.js
+++ 2. Js_basics/2.11,12/2.12.4.js
@@ -1,4 +1,13 @@
+
 var a = prompt('enter the value of a', '')
+
 var b = prompt('enter the value of b', '')
+
 var result = (a + b)
-result = (a + b < 4) ? alert('a few') : alert('a lot')
+
+var message
+
+
+(result < 4) ? (message = 'few') : (message = 'lot')
+
+alert (message)
